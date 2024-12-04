b7066c285dd6085ad0182b1d9c6b3e97517679c8
k4west <kwestmin@gmail.com>
2024-12-05 00:02:39 +0900

    custom_tool.py 생성; 간단한 도구 만들기 테스트

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # agent_practice
 llm agent practice
+
+tools
+    L custom_tools.py
+main.py
+requirements.txt
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from langchain_teddynote import logging
 import warnings
 
+from agent_tools.custom_tools import *
+
 load_dotenv()
 logging.langsmith("agent-practice")
 warnings.filterwarnings("ignore") # 경고 메시지 무시
